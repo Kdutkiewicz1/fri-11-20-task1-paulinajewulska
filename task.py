101f97b941119dda570b80870fb1e99d61fe047d
paulinajewulska <paulina.jewulska@gmail.com>
2020-04-01 23:21:15 +0200

    Refactor Bank class

--- task.py
+++ task.py
@@ -1,51 +1,70 @@
 class Client:
-    def __init__(self, name: str, surname: str, cash: float):
+    def __init__(self, name: str, surname: str):
         self.name = name
         self.surname = surname
+
+    def __hash__(self):
+        return hash((self.name, self.surname))
+
+    def __eq__(self, other):
+        return (self.name, self.surname) == (other.name, other.surname)
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.surname)
+
+
+class Account:
+    def __init__(self, cash: float):
         self.cash = cash
 
 
 class Bank:
     def __init__(self):
-        self.clients: list = [Client("Jan", "Kowalski", 1000)]
+        self.clients: dict = {}
 
-    def add_client(self, name: str, surname: str, cash: float):
-        self.clients.append(Client(name, surname, cash))
+    def add_client(self, new_client: Client, cash: float):
+        self.clients[new_client] = Account(cash)
 
-    def add_cash(self, name: str, surname: str, cash: float, added_cash: float):
-        for client in self.clients:
-            if client.name is name and client.surname is surname and client.cash is cash:
-                client.cash += added_cash
+    def add_cash(self, client: Client, cash_to_add: float):
+        self.clients[client].cash += cash_to_add
 
-    def remove_cash(self, name: str, surname: str, cash: float, removed_cash: float):
-        for client in self.clients:
-            if client.name is name and client.surname is surname and client.cash == cash:
-                if client.cash >= removed_cash:
-                    client.cash -= removed_cash
-                elif client.cash < removed_cash:
-                    print("Not enough money")
+    def remove_cash(self, client: Client, cash_to_remove: float):
+        if self.clients[client].cash < cash_to_remove:
+            print("Not enough cash to remove\n")
+        else:
+            self.clients[client].cash -= cash_to_remove
 
-    def transfer_money(self, client_1_name: str, client_1_surname: str, client_1_cash: float, client_2_name: str,
-                       client_2_surname: str, client_2_cash: float, transfered_cash: float):
-        for client in self.clients:
-            if client.name is client_1_name and client.surname is client_1_surname and client.cash == client_1_cash:
-                for another_client in self.clients:
-                    if client.name is client_2_name and client.surname is client_2_surname and client.cash == client_2_cash:
-                        if client.cash >= transfered_cash:
-                            another_client.cash += transfered_cash
-                            client.cash -= another_client
-                        else:
-                            print("Not enough money")
+    def transfer_money(self, client_from, client_to, cash_to_transfer):
+        if self.clients[client_from].cash < cash_to_transfer:
+            print("Not enough cash to transfer\n")
+        else:
+            self.clients[client_from].cash -= cash_to_transfer
+            self.clients[client_to].cash += cash_to_transfer
+
+    def __str__(self):
+        result = ""
+        for client, account in self.clients.items():
+            result += ("{} {}: {}\n".format(client.surname, client.name, account.cash))
+        return result
 
 
 if __name__ == '__main__':
+
     bank = Bank()
-    bank.add_client("Marek", "Nowak", 2000)
-    bank.add_client("Weronika", "Kowalska", 1200)
-    bank.add_client("Agnieszka", "Kowalska", 3000)
-    bank.add_cash("Agnieszka", "Kowalska", 3000, 100)
-    bank.remove_cash("Agnieszka", "Kowalska", 3100, 1000)
-    bank.remove_cash("Agnieszka", "Kowalska", 2100, 4000)
-    bank.transfer_money("Marek", "Nowak", 2000, "Weronika", "Kowalska", 1200, 100)
-    for client in bank.clients:
-        print("{}, {}, {}".format(client.name, client.surname, client.cash))
+    marek_nowak = Client("Marek", "Nowak")
+    weronika_kowalska = Client("Weronika", "Kowalska")
+    agnieszka_kowalska = Client("Agnieszka", "Kowalska")
+
+    bank.add_client(marek_nowak, cash=2000)
+    bank.add_client(weronika_kowalska, cash=2000)
+    bank.add_client(agnieszka_kowalska, cash=2000)
+    bank.add_cash(marek_nowak, cash_to_add=1000)
+    bank.remove_cash(weronika_kowalska, cash_to_remove=100)
+    bank.transfer_money(client_from=agnieszka_kowalska, client_to=marek_nowak, cash_to_transfer=500)
+
+    print(bank)
+
+    bank_2 = Bank()
+    anna_nowak = Client("Anna", "Nowak")
+    bank_2.add_client(anna_nowak, cash=2000)
+    print(bank_2)
